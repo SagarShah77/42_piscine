d4c023abda53bb2ef94d3bc52cf2788f3a9d12bc
Sagar Shah <shah7sagar7@gmail.com>
2018-04-13 15:31:37 -0700

    delete

--- Day00/ex05/people.sh
+++ Day00/ex05/people.sh
@@ -1,2 +1,2 @@
- #!/bin/sh
-ldapsearch -x -L "(cn=z*)" cn | grep '^cn:' | sort -r --ignore-case | cut -c 5-99
+#!/bin/sh/
+ldapsearch -x -LLL "(cn=z*)" cn | grep '^cn:' | sort  -r --ignore-case | cut -c 5-99
