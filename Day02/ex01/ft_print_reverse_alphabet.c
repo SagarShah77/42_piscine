e119dba7f4ad31b0a9dd80a0eaecb290dce4dc74
Sagar Shah <shah7sagar7@gmail.com>
2018-04-18 06:59:27 -0700

    displays the alphabet in lowercase, from 'z' to 'a'

---
+++ Day02/ex01/ft_print_reverse_alphabet.c
@@ -0,0 +1,25 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_print_reverse_alphabet.c                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: sshah <marvin@42.fr>                       +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2018/03/14 15:36:24 by sshah             #+#    #+#             */
+/*   Updated: 2018/03/14 23:25:21 by sshah            ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+int		ft_putchar(char c);
+
+void	ft_print_reverse_alphabet(void)
+{
+	char letter;
+
+	letter = 'z';
+	while (letter >= 'a')
+	{
+		ft_putchar(letter);
+		letter = letter - 1;
+	}
+}
