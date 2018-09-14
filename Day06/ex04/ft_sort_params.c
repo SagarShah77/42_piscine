859189a272b7a4ba6edbb31707a45ad05460458e
sagarshah77 <shah7sagar7@gmail.com>
2018-09-14 13:03:57 -0700

    Sort the parameter you passes with the output file

---
+++ Day06/ex04/ft_sort_params.c
@@ -0,0 +1,76 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_sort_params.c                                   :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: sshah <marvin@42.fr>                       +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2018/03/21 16:51:33 by sshah             #+#    #+#             */
+/*   Updated: 2018/03/21 17:33:36 by sshah            ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+int		ft_putchar(char c);
+
+int		ft_strcmp(char *s1, char *s2)
+{
+	int i;
+
+	i = 0;
+	while (s1[i] || s2[i])
+	{
+		if (s1[i] == s2[i])
+			i++;
+		if (s1[i] - s2[i] < 0)
+			return (s1[i] - s2[i]);
+		if (s1[i] - s2[i] > 0)
+			return (s1[i] - s2[i]);
+	}
+	return (0);
+}
+
+void	ft_swap(char **a, char **b)
+{
+	char *sagar;
+
+	sagar = *a;
+	*a = *b;
+	*b = sagar;
+}
+
+void	ft_putstr(char *str)
+{
+	int a;
+
+	a = 0;
+	while (str[a])
+	{
+		ft_putchar(str[a]);
+		a++;
+	}
+}
+
+int		main(int argc, char **argv)
+{
+	int i;
+
+	i = 1;
+	while (i < argc - 1)
+	{
+		if (ft_strcmp(argv[i], argv[i + 1]) > 0)
+		{
+			ft_swap(&argv[i], &argv[i + 1]);
+			i = 1;
+		}
+		else
+			i++;
+	}
+	i = 1;
+	while (i < argc)
+	{
+		ft_putstr(argv[i]);
+		ft_putchar('\n');
+		i++;
+	}
+	return (0);
+}
