53bf121c2333c666eaa7b1613a69f5703ce98f3a
sagarshah77 <shah7sagar7@gmail.com>
2018-09-14 13:08:53 -0700

    Strdup Function

---
+++ Day07/ex00/ft_strdup.c
@@ -0,0 +1,35 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strdup.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: sshah <marvin@42.fr>                       +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2018/03/21 19:31:47 by sshah             #+#    #+#             */
+/*   Updated: 2018/03/21 19:53:13 by sshah            ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <stdlib.h>
+
+char	*ft_strdup(char *src)
+{
+	int		i;
+	int		s;
+	char	*str;
+
+	i = 0;
+	s = 0;
+	while (src[s])
+		s++;
+	str = (char*)malloc((sizeof(src) * s) + 1);
+	if (str == NULL)
+		return (NULL);
+	while (src[i])
+	{
+		str[i] = src[i];
+		i++;
+	}
+	str[i] = '\0';
+	return (str);
+}
