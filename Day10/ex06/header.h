0b85420fe1c5ff1d38e2d596eca53852bbc9ba65
sagarshah77 <shah7sagar7@gmail.com>
2018-09-19 15:31:23 -0700

    Create a Program called do-op (Read the question from PDF)

---
+++ Day10/ex06/header.h
@@ -0,0 +1,24 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   header.h                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: sshah <marvin@42.fr>                       +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2018/03/26 23:41:54 by sshah             #+#    #+#             */
+/*   Updated: 2018/03/26 23:50:09 by sshah            ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef HEADER_H
+# define HEADER_H
+
+# include <unistd.h>
+
+int		ft_sum(int n1, int n2);
+int		ft_sub(int n1, int n2);
+int		ft_mul(int n1, int n2);
+int		ft_div(int n1, int n2);
+int		ft_mod(int n1, int n2);
+
+#endif
