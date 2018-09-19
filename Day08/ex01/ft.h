a1ec4dc49f011a84fee6eb36b99e2a08d83840d4
sagarshah77 <shah7sagar7@gmail.com>
2018-09-19 15:13:22 -0700

    Create Header file that contains prototype of libft.a

---
+++ Day08/ex01/ft.h
@@ -0,0 +1,21 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft.h                                               :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: sshah <marvin@42.fr>                       +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2018/03/22 17:58:22 by sshah             #+#    #+#             */
+/*   Updated: 2018/03/22 21:07:43 by sshah            ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef FT_H
+# define FT_H
+
+void	ft_putchar(char c);
+void	ft_putstr(char *str);
+void	ft_swap(int *a, int *b);
+int		ft_strcmp(char *s1, char *s2);
+int		ft_strlen(char *str);
+#endif
