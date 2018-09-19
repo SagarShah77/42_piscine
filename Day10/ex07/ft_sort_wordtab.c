62b7e0e7fd5bccd703940d5ff11a3e9b46bf8bb5
sagarshah77 <shah7sagar7@gmail.com>
2018-09-19 15:31:50 -0700

    Create a function (Read the question from PDF)

---
+++ Day10/ex07/ft_sort_wordtab.c
@@ -0,0 +1,67 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_sort_wordtab.c                                  :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: sshah <marvin@42.fr>                       +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2018/03/27 21:25:19 by sshah             #+#    #+#             */
+/*   Updated: 2018/03/27 21:27:22 by sshah            ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+int		ft_strcmp(char *s1, char *s2)
+{
+	int i;
+
+	i = 0;
+	while (s1[i] == s2[i])
+	{
+		if (s1[i] == '\0' && s2[i] == '\0')
+			return (0);
+		i++;
+	}
+	return (s1[i] - s2[i]);
+}
+
+void	ft_swap(char **a, char **b)
+{
+	char *swap;
+
+	swap = *a;
+	*a = *b;
+	*b = swap;
+}
+
+int		getlength(char **tab)
+{
+	int i;
+
+	i = 0;
+	while (tab[i] != 0)
+		i++;
+	return (i);
+}
+
+void	ft_sort_wordtab(char **tab)
+{
+	int i;
+	int j;
+	int size;
+
+	i = 0;
+	size = getlength(tab);
+	while (i < size - 1)
+	{
+		j = 0;
+		while (j < size - i - 1)
+		{
+			if (ft_strcmp(tab[j], tab[j + 1]) > 0)
+			{
+				ft_swap(&tab[j], &tab[j + 1]);
+			}
+			j++;
+		}
+		i++;
+	}
+}
